6a1eaa6c98f7d3e3fc26c13c6dce9c67ae904cfd
aka <aaronkaltman@gmail.com>
2023-12-28 16:48:17 -0500

    updating

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,9 +8,10 @@ export default function Home() {
       <h1 className="text-5xl font-bold py-5 tracking-tight leading-tight">
         Clever AI - Smart Notes Application
       </h1>
-      <p className="py-3 text-2xl tracking-normal leading-tight">
+      <p className="py-3 text-2xl tracking-tight leading-10">
         Clever Notes AI is a smart notes application that uses artificial
-        intelligence to help you take notes. It is powered by{" "}
+        intelligence to help you take notes. It is powered by React, NextJS,
+        Prisma, MongoDB, Clerk, and more.{" "}
         <a href="https://bigboss.ai">Big Boss Enterprises</a>.
       </p>
       <Link href="/notes" className="my-3">
