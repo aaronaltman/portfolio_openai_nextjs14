cffd00fe6730c1eacab8c8fa9b2d39beee065609
aka <aaronkaltman@gmail.com>
2023-12-28 21:31:40 -0500

    baby hour

--- src/api/posts/route.ts
+++ src/api/posts/route.ts
@@ -1,5 +1,47 @@
-// Import PrismaClient
-import { PrismaClient } from "@prisma/client";
+// src/api/posts/route.ts
 
-// Instantiate PrismaClient
+import { PrismaClient } from "@prisma/client";
+import type { NextApiRequest, NextApiResponse } from "next";
+
 const prisma = new PrismaClient();
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  switch (req.method) {
+    case "GET":
+      // Retrieve all posts
+      try {
+        const posts = await prisma.post.findMany();
+        res.status(200).json(posts);
+      } catch (error) {
+        res.status(500).json({ error: "Failed to fetch posts" });
+      }
+      break;
+
+    case "POST":
+      // Create a new post
+      try {
+        const { title, content, authorId } = req.body; // Make sure to validate and sanitize in real apps!
+        const newPost = await prisma.post.create({
+          data: {
+            title,
+            content,
+            authorId,
+          },
+        });
+        res.status(201).json(newPost);
+      } catch (error) {
+        res.status(500).json({ error: "Failed to create post" });
+      }
+      break;
+
+    // You can add more cases for other methods (PUT, DELETE) as needed.
+
+    default:
+      // Handle any unhandled HTTP methods
+      res.setHeader("Allow", ["GET", "POST"]);
+      res.status(405).end(`Method ${req.method} Not Allowed`);
+  }
+}
