c13c6fee1b1e5cfe7984df90aa2d00bc3398307f
aka <aaronkaltman@gmail.com>
2023-12-28 20:38:16 -0500

    should work now

--- src/app/notes/_components/add-note-dialogue.tsx
+++ src/app/notes/_components/add-note-dialogue.tsx
@@ -2,17 +2,6 @@ import { Form } from "@/components/ui/form";
 import { CreateNoteSchema } from "@/lib/validation/note";
 import { zodResolver } from "@hookform/resolvers/zod";
 
-type AddNoteDialogueProps = {
-  open: boolean;
-  setOpen: (open: boolean) => void;
-};
-export default function AddNoteDialogue({
-  open,
-  setOpen,
-}: AddNoteDialogueProps) {
-  const form = useForm<CreateNoteSchema>({
-    resolver: zodResolver(CreateNoteSchema),
-  });
-
+export default function AddNoteDialogue() {
   return <div>add-note-dialogue</div>;
 }
